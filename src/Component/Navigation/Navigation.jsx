8208218f6e747f6bc691c42aca02846fc1580901
adhaghani <adhaahmad04@gmail.com>
2024-06-03 15:01:47 +0800

    Updated, Navigation now Mobile responsive

--- src/Component/Navigation/Navigation.jsx
+++ src/Component/Navigation/Navigation.jsx
@@ -7,6 +7,7 @@ import linkedin from "/link.svg";
 const Navigation = () => {
   const [Sidebar, setSidebar] = useState(false);
 
+  const showSidebar = () => setSidebar(!Sidebar);
   return (
     <div className="Navigation" id="Navigation">
       <div className="Navigation-Container">
@@ -36,6 +37,48 @@ const Navigation = () => {
             </a>
           </li>
         </ul>
+        <button
+          className={Sidebar ? "hamburger active" : "hamburger"}
+          id="hamburger"
+          onClick={showSidebar}
+        >
+          <div className="top"></div>
+          <div className="mid"></div>
+          <div className="bot"></div>
+        </button>
+      </div>
+      <div className={Sidebar ? "SideNav active" : "SideNav"} id="SideNav">
+        <div className="SideNav-Container">
+          <ul className="Shortcurt">
+            <li>
+              <a href="#About" onClick={showSidebar}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#Project" onClick={showSidebar}>
+                Project
+              </a>
+            </li>
+            <li>
+              <a href="#Contact" onClick={showSidebar}>
+                Contact
+              </a>
+            </li>
+          </ul>
+          <ul className="Link">
+            <li>
+              <a href="https://github.com/adhaghani" target="_BLANK">
+                <img src={github} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/adhaghani/" target="_BLANK">
+                <img src={linkedin} alt="" />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/eportfolio/",
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true
+  }
 });
