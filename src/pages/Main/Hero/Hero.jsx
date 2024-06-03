f037d76d95d5e2478df49f1c089918097376de03
adhaghani <adhaahmad04@gmail.com>
2024-06-03 11:44:03 +0800

    Udpated Hero Component.

--- src/pages/Main/Hero/Hero.jsx
+++ src/pages/Main/Hero/Hero.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+import "./Hero.css";
+
+import profile from "/memoji.png";
+import Button from "../../../Component/Button/Button";
 const Hero = () => {
   return (
     <div className="Hero" id="Hero">
-      Hero
+      <div className="Hero-Container">
+        <div className="greetings-container">
+          <h1>
+            Hello, I'm <span>Adha</span>!
+          </h1>
+          <h2>
+            A computer science Student passionated in Software Development.
+          </h2>
+        </div>
+        <div className="icon-container">
+          <div className="image-container">
+            <img src={profile} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
