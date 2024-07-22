3b7603baf19c2045ff815a40b004cfa935e3009e
adhaghani <adhaahmad04@gmail.com>
2024-07-22 14:46:54 +0800

    Overhaul Navigation, Added Logo to HTML, Increase Font size for Nav List.

--- src/Component/Navigation/Navigation.jsx
+++ src/Component/Navigation/Navigation.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../style/Navigation/Navigation.css";
 import { Link } from "react-router-dom";
+
+import Logo from "/icon.svg";
 const Navigation = () => {
+  const [Sidebar, setSidebar] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -9,25 +13,63 @@ const Navigation = () => {
     });
   };
 
+  const handleSideNav = () => {
+    setSidebar(!Sidebar);
+  };
   return (
     <nav>
-      <ul className="List">
-        <li>
-          <Link to="/Home" onClick={scrollToTop}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/About" onClick={scrollToTop}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link to="/Projects" onClick={scrollToTop}>
-            Projects
-          </Link>
-        </li>
-      </ul>
+      <div className="Nav-Container">
+        <div className="Logo_Container">
+          <img src={Logo} alt="" className="logo" />
+        </div>
+
+        <ul className="List">
+          <li>
+            <Link to="/Home" onClick={scrollToTop}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/About" onClick={scrollToTop}>
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link to="/Projects" onClick={scrollToTop}>
+              Projects
+            </Link>
+          </li>
+        </ul>
+        <button
+          className={Sidebar ? "Hamburger Active" : "Hamburger"}
+          id="Hamburger"
+          onClick={handleSideNav}
+        >
+          <div className="Top"></div>
+          <div className="Mid"></div>
+          <div className="Bot"></div>
+        </button>
+      </div>
+
+      <div className={Sidebar ? "SideNav Active" : "SideNav"}>
+        <ul className="List">
+          <li>
+            <Link to="/Home" onClick={(scrollToTop, handleSideNav)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/About" onClick={(scrollToTop, handleSideNav)}>
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link to="/Projects" onClick={(scrollToTop, handleSideNav)}>
+              Projects
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
